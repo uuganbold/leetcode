cf8cd75fdebc9d5623a7ff783dc15310cc048aa4
uuganbold <uuganhuu@gmail.com>
2020-04-30 16:39:12 -0500

    problem 1365 version 2 O(n)

--- java/Solution1365.java
+++ java/Solution1365.java
@@ -1,16 +1,26 @@
 class Solution1365 {
+    /*
+    T: O(n)
+    S: O(101)=O(1)
+    */
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        /*
-        T:O(n^2)
-        S:O(1)
-        */
         int[] result=new int[nums.length];
+        
+        int[] freq=new int[101];
         for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]<nums[i]) count+=1;
+            freq[nums[i]]+=1;
+        }
+        
+        for(int i=1;i<freq.length;i++){
+            freq[i]+=freq[i-1];
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                result[i]=0;
+            }else{
+                result[i]=freq[nums[i]-1];
             }
-            result[i]=count;
         }
         return result;
     }
