4dc8e2f71ff6dea1dce079a73e26232144692bf4
uuganbold <uuganhuu@gmail.com>
2020-04-30 18:37:34 -0500

    problem 938 iterative

--- java/Solution938.java
+++ java/Solution938.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class Solution938 {
     public class TreeNode {
         int val;
@@ -16,6 +18,19 @@ class Solution938 {
         if(root.val>=L&&root.val<=R) sum+=root.val;
         if(root.val>L) sum+=rangeSumBST(root.left,L,R);
         if(root.val<R) sum+=rangeSumBST(root.right,L,R);
+        return sum;
+    }
+
+    public int rangeSumBSTIterative(TreeNode root, int L, int R) {
+        int sum=0;
+        Stack<TreeNode> stack=new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()s){
+            TreeNode node=stack.pop();
+            if(node.val>=L&&root.val<=R) sum+=node.val;
+            if(root.val>L) stack.push(root.left);
+            if(root.val<R) stack.push(root.right);
+        }
         return sum;
     }
 }
