6f1239f02ceb0fd9b5eae0937301b60fa6ae2c7e
uuganbold <uuganhuu@gmail.com>
2020-05-04 15:22:07 -0500

    problem 617

---
+++ java/Solution617.java
@@ -0,0 +1,29 @@
+public class Solution617 {
+     public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /*
+    T: O(N)
+    S: O(N)
+    */
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1==null) return t2;
+        if(t2==null) return t1;
+        
+        TreeNode root=new TreeNode(t1.val+t2.val);
+        root.left=mergeTrees(t1.left,t2.left);
+        root.right=mergeTrees(t1.right,t2.right);
+        return root;
+    }
+    
+}
