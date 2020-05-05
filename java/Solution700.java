7a38f56dcf54502d7c8fb8e99852db8eb188a151
uuganbold <uuganhuu@gmail.com>
2020-05-05 16:08:55 -0500

    problem 700

---
+++ java/Solution700.java
@@ -0,0 +1,32 @@
+class Solution700 {
+
+    public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode() {}
+            TreeNode(int val) { this.val = val; }
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+    }
+    public TreeNode searchBSTRecursive(TreeNode root, int val) {
+        if(root==null) return null;
+        if(root.val==val) return root;
+        if(val>root.val){
+            return searchBSTRecursive(root.right,val);
+        }else return searchBSTRecursive(root.left,val);
+    }
+    
+    public TreeNode searchBST(TreeNode root, int val){
+        while(root!=null){
+            if(root.val==val) return root;
+            if(root.val>val) root=root.left;
+            else root=root.right;
+        }
+        
+        return null;
+    }
+}
