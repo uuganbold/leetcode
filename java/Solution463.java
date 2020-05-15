ee39293fb063ccd8944154b591f4dc3f2376384d
uuganbold <uuganhuu@gmail.com>
2020-05-14 23:00:18 -0500

    problem 463

---
+++ java/Solution463.java
@@ -0,0 +1,23 @@
+class Solution463 {
+
+    /*
+        T: O(N)
+        S: O(1)
+    */
+    public int islandPerimeter(int[][] grid) {
+        int perimeter=0;
+        
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==1){
+                    if(i-1<0||grid[i-1][j]==0) perimeter++;
+                    if(i+1>=grid.length||grid[i+1][j]==0) perimeter++;
+                    if(j-1<0||grid[i][j-1]==0) perimeter++;
+                    if(j+1>=grid[i].length||grid[i][j+1]==0) perimeter++;
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+}
