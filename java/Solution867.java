51c7d733db166fd714402f08f8c68d7362f82348
uuganbold <uuganhuu@gmail.com>
2020-05-14 23:04:05 -0500

    problem 867

---
+++ java/Solution867.java
@@ -0,0 +1,18 @@
+class Solution867 {
+
+    /*
+        O(R*C)
+        O(1)
+    */
+    public int[][] transpose(int[][] A) {
+        int[][] result=new int[A[0].length][A.length];
+        
+        for(int i=0;i<A.length;i++){
+            for(int j=0;j<A[i].length;j++){
+                result[j][i]=A[i][j];
+            }
+        }
+        
+        return result;
+    }
+}
