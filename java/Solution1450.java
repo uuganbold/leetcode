9ff4912e4edd63d312f6b9dab1a1d229b3217e83
uuganbold <uuganhuu@gmail.com>
2020-05-16 23:15:45 -0500

    complexity

--- java/Solution1450.java
+++ java/Solution1450.java
@@ -1,4 +1,9 @@
 class Solution1450 {
+
+    /*
+        T: O(N)
+        S: O(1)
+    */
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
         int count=0;
         for(int i=0;i<startTime.length;i++){
