51db95fc014a607d2cad201d7ab6a1b651903e74
uuganbold <uuganhuu@gmail.com>
2020-05-26 19:39:44 -0500

    problem 21

---
+++ java/Solution21.java
@@ -0,0 +1,48 @@
+class Solution21 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /*
+        T: O(N)
+        S: O(1)
+    */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode ans=new ListNode();
+        ListNode cur=ans;
+        
+        while(l1!=null&&l2!=null){
+            if(l1.val>l2.val){
+                cur.next=l2;
+                l2=l2.next;
+            }else{
+                cur.next=l1;
+                l1=l1.next;
+            }
+            cur=cur.next;
+        }
+        
+        if(l1!=null){
+            cur.next=l1;
+        }
+        if(l2!=null){
+            cur.next=l2;
+        }
+        
+        return ans.next;
+    }
+}
