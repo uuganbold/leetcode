acfc927c6f8745e8ab0ef90a585535aa2b0fc19d
uuganbold <uuganhuu@gmail.com>
2020-05-28 21:52:14 -0500

    problem 371

---
+++ java/Solution371.java
@@ -0,0 +1,20 @@
+class Solution371 {
+
+    /*
+        T: O(32)
+        S: O(1)
+    */
+    public int getSum(int a, int b) {
+        
+        while(b!=0){
+            int carry=a&b;
+
+            a=a^b;
+
+            b=carry<<1 ;           
+        }
+
+        return a;
+        
+    }
+}
