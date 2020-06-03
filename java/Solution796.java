4aac256c70b7af188e810a500d4b8e5ec4efbde3
uuganbold <uuganhuu@gmail.com>
2020-06-02 21:31:52 -0500

    added KMP algorithm

--- java/Solution796.java
+++ java/Solution796.java
@@ -10,4 +10,47 @@ class Solution796 {
         if(A.length()!=B.length()) return false;
         return (A+A).indexOf(B)>=0;
     }
+
+    public int KMPAlgorithm(String A, String B){
+        int[] shift=KMPPatternShift(B);
+       
+       int a=0;
+       int b=0;
+       while(a<A.length()&&b<B.length()){
+           if(A.charAt(a)==B.charAt(b)){
+               a++;
+               b++;
+           }else{
+               if(b!=0){
+                   b=shift[b-1];
+               }else{
+                   a++;
+               }
+           }
+       }
+       
+       if(b==B.length()){
+           return a-b;
+       }
+       
+       return -1;
+   }
+   
+   private int[] KMPPatternShift(String str){
+       int[] ans=new int[str.length()];
+       
+       int left=0;
+       for(int right=1;right<str.length();right++){
+           while(left>0&&str.charAt(left)!=str.charAt(right)){
+               left=ans[left-1];
+           }
+           if(str.charAt(left)!=str.charAt(right)){
+                ans[right]=0;
+           }else{
+               ans[right]=++left;
+           }
+       }
+       
+       return ans;
+   }
 }
