4b1cd3f9cbf67fac16647c574a0cbfcf05c4ac05
uuganbold <uuganhuu@gmail.com>
2020-06-04 00:58:31 -0500

    problem 836

---
+++ java/Solution836.java
@@ -0,0 +1,15 @@
+class Solution836 {
+
+    /*
+        T: O(1)
+        S: O(1)
+    */
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return !(
+            rec1[2]<=rec2[0]||
+            rec1[3]<=rec2[1]||
+            rec1[0]>=rec2[2]||
+            rec1[1]>=rec2[3]
+        );
+    }
+}
