fb2c6a1eb1cbac30cf8edc5183139a2d6cfde637
uuganbold <uuganhuu@gmail.com>
2020-06-09 00:54:45 -0500

    problem 26

---
+++ java/Solution26.java
@@ -0,0 +1,20 @@
+class Solution26 {
+    
+    /*
+        T: O(N)
+        S: O(1)
+    */
+    public int removeDuplicates(int[] nums) {
+        if(nums==null|nums.length==0){
+            return 0;
+        }
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
