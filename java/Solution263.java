b89a80f6e8c31bd80926fbc731e29660a4f4ea10
uuganbold <uuganhuu@gmail.com>
2020-06-11 17:38:43 -0500

    problem 263

---
+++ java/Solution263.java
@@ -0,0 +1,26 @@
+class Solution263 {
+
+    /*
+        T: O(Log)
+        S: O(1)
+    */
+    public boolean isUgly(int num) {
+         if(num<1){
+             return false;
+         }
+         
+         while(num%2==0){
+             num/=2;
+         }
+        
+         while(num%3==0){
+             num/=3;
+         }
+        
+         while(num%5==0){
+             num/=5;
+         }
+        
+        return num==1;
+    }
+}
