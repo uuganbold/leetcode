5a0b2db1b22148ecde25ea442bbd870b382d06db
uuganbold <uuganhuu@gmail.com>
2020-06-11 21:24:39 -0500

    problem 443

---
+++ java/Solution443.java
@@ -0,0 +1,45 @@
+class Solution443 {
+
+    /*
+        T: O(N)
+        S: O(1)
+    */
+    public int compress(char[] chars) {
+        int write=0;
+        int lastChar=0;
+        
+        for(int read=0;read<=chars.length;read++){
+            if(read==chars.length||chars[read]!=chars[lastChar]){
+                 int count=read-lastChar;
+                 chars[write++]=chars[lastChar];
+                 write=putCountInPlace(chars,write,count);
+                 lastChar=read;
+            }
+        }
+        
+        return write;
+    }
+    
+    private int putCountInPlace(char[] chars, int start,int count){
+        if(count==1){
+            return start;
+        }
+        
+        int end=start;
+        while(count>0){
+            chars[end++]=(char)(count%10+'0');
+            count/=10;
+        }
+        int ans=end;
+        while(start<end-1){
+            char temp=chars[start];
+            chars[start]=chars[end-1];
+            chars[end-1]=temp;
+            start++;
+            end--;
+        }
+        
+        return ans;
+    }
+
+}
