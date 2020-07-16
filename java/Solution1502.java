675c8184ea3ae014b39257ad305d032522a785af
uuganbold <uuganhuu@gmail.com>
2020-07-16 00:31:39 -0500

    fix

--- java/Solution1502.java
+++ java/Solution1502.java
@@ -1,14 +1,15 @@
+import java.util.*;
+
 class Solution1502 {
-    
+
     /*
-        T: O(nlogn)
-        S: O(1)
-    */
+     * T: O(nlogn) S: O(1)
+     */
     public boolean canMakeArithmeticProgression(int[] arr) {
         Arrays.sort(arr);
-        int diff=arr[1]-arr[0];
-        for(int i=2;i<arr.length;i++){
-            if(arr[i]-arr[i-1]!=diff){
+        int diff = arr[1] - arr[0];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] != diff) {
                 return false;
             }
         }
