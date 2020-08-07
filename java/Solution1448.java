2a0ce3735384761d818386a8c3307e11ace00fda
uuganbold <uuganhuu@gmail.com>
2020-08-06 22:55:23 -0500

    problem 1448

--- java/Solution1448.java
+++ java/Solution1448.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * Definition for a binary tree node. public class TreeNode { int val; TreeNode
  * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
@@ -23,6 +25,41 @@ class Solution1448 {
         }
         ans += helper(root.left, currentGreatest);
         ans += helper(root.right, currentGreatest);
+        return ans;
+    }
+
+    /*
+     * T: O(N) S: O(logN)
+     */
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int ans = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> greates = new Stack<>();
+        stack.push(root);
+        greates.push(Integer.MIN_VALUE);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            Integer great = greates.pop();
+
+            if (node.val >= great) {
+                ans++;
+                great = node.val;
+            }
+
+            if (node.left != null) {
+                stack.push(node.left);
+                greates.push(great);
+            }
+
+            if (node.right != null) {
+                stack.push(node.right);
+                greates.push(great);
+            }
+        }
+
         return ans;
     }
 
